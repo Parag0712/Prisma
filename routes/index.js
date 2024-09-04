ab3fce9ee09751e8780281bd481118e238735288
Parag0712 <paragvadgama123@gmail.com>
2024-09-04 14:39:04 +0530

    router added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
 import UserRoutes from "./userRoute.js";
+import CommentRoutes from "./commentRoute.js";
+import PostRoutes from "./postRoute.js";
 const router = Router();
 
 router.use("/api/user", UserRoutes);
+
+// * For Post Routes
+router.use("/api/post", PostRoutes);
+
+// * For Post Routes
+router.use("/api/comment", CommentRoutes);
 
 export default router;
